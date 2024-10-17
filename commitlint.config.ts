332c9a659be0345fb13871e05c5175f8dd03fc42
Zhang YingJie <jie736805161@qq.com>
2024-10-17 13:45:18 +0800

    feat: save --scope=123 --user=123

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -3,9 +3,20 @@ export default {
   extends: ["@commitlint/config-conventional", "cz"],
   plugins: [
     {
+      // 校验提交信息是否包含 scope 和 user 参数
       rules: {
         "examine-subject": (parsed) => {
-          console.log(parsed.subject, "subject-----");
+          const containsScopeFlag =
+            parsed.subject.includes("--scope") ||
+            parsed.subject.includes("--bug");
+          if (!containsScopeFlag) {
+            return [false, '提交信息必须包含 "--scope" 或者 "--bug" 参数'];
+          }
+          const containsUserFlag = parsed.subject.includes("--user");
+          if (!containsUserFlag) {
+            return [false, '提交信息必须包含 "--user" 参数'];
+          }
+
           return [true];
         },
       },
